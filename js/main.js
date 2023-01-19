d003fd3fd01527f95dd0c25d6407af383296d144
Caio Silveira <contato.caionunes@gmail.com>
2023-01-19 20:13:07 -0300

    v2

--- js/main.js
+++ js/main.js
@@ -5,6 +5,51 @@ let scoreGuest = document.getElementById("score-guest")
 let highHome = document.getElementById("score-wrap-home")
 let highGuest = document.getElementById("score-wrap-guest")
 
+function highlight() {
+    if (countHome > countGuest) {  
+        highHome.style.border = "3px solid #F94F6D"
+        highGuest.style.border = "none"
+    }
+    else if (countGuest > countHome) {
+        highGuest.style.border = "3px solid #F94F6D"
+        highHome.style.border = "none"
+    }
+    else if (countHome === 0 && countGuest === 0) {
+        highHome.style.border = "none"
+        highGuest.style.border = "none"
+    }
+    else {
+        highHome.style.border = "3px solid #e6e600"
+        highGuest.style.border = "3px solid #e6e600"
+    }
+}
+
+function newGame() {
+    countHome = 0
+    countGuest = 0
+    scoreHome.innerText = countHome
+    scoreGuest.innerText = countGuest
+    highlight()
+}
+
+function plusPoints(a, b) {
+    if (a == 'home') {
+        countHome = countHome + b
+        scoreHome.innerText = countHome
+    } else if (a == 'guest') {
+        countGuest = countGuest + b
+        scoreGuest.innerText = countGuest
+    } else {
+        alert('Something\'s wrong');
+    }
+    highlight()
+}
+
+
+/*
+Old code to compare
+
+
 // Highlight the leader
 
 function highlight() {
@@ -71,3 +116,4 @@ function plusThreeGuest() {
     scoreGuest.innerText = countGuest
     highlight()
 }
+*/
